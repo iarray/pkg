5ac470505e6f32cc44cca91e647e5d688ec819ec
iarray <286423109@qq.com>
2020-01-04 09:23:53 +0800

    r

---
+++ tools/rpcclient/rpc_client.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"net/rpc"
+	"sync"
+)
+
+var client *rpc.Client
+var nw, remoteAddr string
+var lock sync.Mutex
+
+func setRemoteAddress(netword, addr string) {
+	lock.Lock()
+	defer lock.Unlock()
+	remoteAddr = addr
+	nw = netword
+	if client != nil {
+		client.Close()
+	}
+}
+
+func connect() {
+	if client == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if client == nil {
+			client, _ = rpc.Dial(nw, remoteAddr)
+		}
+	}
+}
+
+func run(method string, args interface{}, result interface{}) error {
+	connect()
+	return client.Call(method, args, result)
+}
